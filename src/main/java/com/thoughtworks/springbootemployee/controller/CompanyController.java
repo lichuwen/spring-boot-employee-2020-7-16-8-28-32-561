059b5b4b62cab37e3da6dadb5f4b497dca019508
lichuwen <545759585@qq.com>
2020-07-28 18:13:45 +0800

    [karen][york] refactor:getAll function's add param

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -2,22 +2,31 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.lang.Integer.parseInt;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
     @GetMapping
-    public List<Company> getAll() {
+    public List<Company> getAll(@RequestParam(name = "page" , required = false) Integer page, @RequestParam(name = "pageSize",required = false) Integer pageSize) {
         List<Company> companies = new ArrayList<>();
+        List<Company> certainCompanies = new ArrayList<>();
         companies.add(new Company(1,null));
         companies.add(new Company(2,null));
+        companies.add(new Company(3,null));
+        companies.add(new Company(4,null));
+        companies.add(new Company(5,null));
+        if(page != null && pageSize != null) {
+            for(int index = 0; index < pageSize; index++) {
+                certainCompanies.add(companies.get(index));
+            }
+            return certainCompanies;
+        }
         return companies;
     }
 
