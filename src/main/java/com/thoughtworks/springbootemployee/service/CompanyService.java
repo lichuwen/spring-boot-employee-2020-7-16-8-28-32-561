e51a083e06cd177dcb6e8ac1c8241dbd7a0d596a
lichuwen <545759585@qq.com>
2020-07-29 14:48:55 +0800

    [karen,henry] feature : add and implement  should_return_certain_company_when_get_certain_company_given_company_id & should_return_employees_when_get_employees_in_company_given_company_id test

--- src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
@@ -2,9 +2,12 @@ package com.thoughtworks.springbootemployee.service;
 
 
 import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 public class CompanyService {
@@ -16,5 +19,17 @@ public class CompanyService {
 
     public List<Company> getAllCompanies() {
         return companyRepository.getAll();
+    }
+
+    public Company getCertainCompany(Integer companyId) {
+        List<Company> companyArraysList =  companyRepository.getAll();
+        return companyArraysList.stream().filter(company -> company.getCompanyID() == companyId).findFirst().orElse(null);
+    }
+
+    public List<Employee> getEmployeesInCompany(Integer companyId) {
+         return companyRepository.getAll().stream()
+                 .filter(company -> company.getCompanyID() == companyId)
+                 .map(Company::getEmployees)
+                 .findFirst().orElse(new ArrayList<>());
     }
 }
--- src/test/java/com/thoughtworks/springbootemployee/service/companyServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/companyServiceTest.java
@@ -3,29 +3,66 @@ package com.thoughtworks.springbootemployee.service;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 public class companyServiceTest {
+    private CompanyRepository companyRepository;
+    private CompanyService companyService;
+
+    @BeforeEach
+    private void init() {
+        companyRepository = Mockito.mock(CompanyRepository.class);
+        when(companyRepository.getAll())
+                .thenReturn(Collections.singletonList(
+                        new Company(1, 2,
+                                Arrays.asList(
+                                        new Employee(1, "xiaoming", "male"),
+                                        new Employee(2, "jeany", "female"),
+                                        new Employee(3, "woody", "male")
+                                ))));
+        companyService = new CompanyService(companyRepository);
+    }
+
     @Test
     public void should_return_companyList_when_get_all_companies() {
         //given
-        CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
-        when(companyRepository.getAll()).thenReturn(Arrays.asList(new Company(1, 2, Arrays.asList(new Employee(1, "xiaoming", "male")))));
-        CompanyService companyService = new CompanyService(companyRepository);
         List<Company> companyList;
-
         //when
         companyList = companyService.getAllCompanies();
-
         //then
         assertTrue(companyList.size() > 0);
     }
+
+    @Test
+    void should_return_certain_company_when_get_certain_company_given_company_id() {
+        //given
+        Integer companyId = 1;
+        //when
+        Company company = companyService.getCertainCompany(companyId);
+        //then
+        assertNotNull(company);
+        assertEquals(companyId, company.getCompanyID());
+    }
+
+    @Test
+    void should_return_employees_when_get_employees_in_company_given_company_id() {
+        //given
+        Integer companyId = 1;
+        //when
+        List<Employee> employees = companyService.getEmployeesInCompany(companyId);
+        //then
+        assertTrue(employees.size() > 0);
+
+    }
+
 
 }
