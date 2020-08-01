ab319f8e6427322b5bb97154e928e56cc71e8c69
lichuwen <545759585@qq.com>
2020-08-01 21:05:32 +0800

    [karen] feature : add @Component to CompanyMapper & EmployeeMapper

--- src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
+++ src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
@@ -1,10 +1,12 @@
-package mapper;
+package com.thoughtworks.springbootemployee.mapper;
 
 import com.thoughtworks.springbootemployee.dto.CompanyRequest;
 import com.thoughtworks.springbootemployee.dto.CompanyResponse;
 import com.thoughtworks.springbootemployee.model.Company;
 import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CompanyMapper {
     public Company toCompany(CompanyRequest companyRequest) {
         Company company = new Company();
